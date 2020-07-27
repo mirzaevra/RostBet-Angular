d8c4e1dd0a692e7b20a11c1e64945fec2d82340b
mirzaev.r.a <mirzaev.r.a@gmail.com>
2020-07-27 11:18:22 +0300

    сохранения id выбранных элементов

--- src/app/games/shared/components/merchant-list/merchant-list.component.ts
+++ src/app/games/shared/components/merchant-list/merchant-list.component.ts
@@ -7,12 +7,35 @@ import {Merchants} from '../../../../shared/interface';
   styleUrls: ['./merchant-list.component.scss']
 })
 export class MerchantListComponent implements OnInit {
+  public merchantIds: any[] = [];
   @Input() merchantList: Merchants[];
 
   constructor() {
   }
 
   ngOnInit(): void {
+  }
+
+  isSelected(merchant): number {
+    let isSelected = -1;
+    this.merchantIds.forEach((item, index) => {
+      if (merchant.ID === item) {
+        isSelected = index;
+      }
+      return false;
+    });
+    return isSelected;
+  }
+
+  toggleMerchant(merchant: Merchants): void {
+    const index = this.isSelected(merchant);
+    console.log(index);
+    if (index < 0) {
+      this.merchantIds.push(merchant.ID);
+    } else {
+      this.merchantIds.splice(index, 1);
+    }
+    console.log(this.merchantIds);
   }
 
 }
